9f5c399e16f33dee5ee51d2aea99b3f0310e4768
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-01 14:01:47 +0330

    change void type to an object in authController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,15 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-import { AuthCredentialsDto } from './entities/auth-credential.dto'
+import { AuthCredentialsDto } from '../auth/dto/auth-credential.dto'
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) {}
 
     @Post('singup')
-    async singUp(@Body() createUserDto: CreateUserDto): Promise<void> {
-        await this.authService.signUP(createUserDto);
+    async singUp(@Body() createUserDto: CreateUserDto): Promise<{ message: string; userId: string }> {
+       return await this.authService.signUp(createUserDto);
     }
 
     @Post('signin')
