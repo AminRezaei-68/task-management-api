121840b8e3827fe329d370f456816769f279d146
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-02 17:57:05 +0330

    Code tidying

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,21 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
-import { AuthCredentialsDto } from '../auth/dto/auth-credential.dto'
+import { AuthCredentialsDto } from '../auth/dto/auth-credential.dto';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('singup')
-    async singUp(@Body() createUserDto: CreateUserDto): Promise<{ message: string; userId: string }> {
-       return await this.authService.signUp(createUserDto);
-    }
+  @Post('singup')
+  async singUp(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<{ message: string; userId: string }> {
+    return await this.authService.signUp(createUserDto);
+  }
 
-    @Post('signin')
-    async signIn(@Body() authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
-        return this.authService.signIn(authCredentialsDto);
-    }
-
-
+  @Post('signin')
+  async signIn(
+    @Body() authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,22 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { PassportModule } from '@nestjs/passport'
+import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { ConfigService, ConfigModule} from '@nestjs/config';
-import { JwtStrategy } from './strategies/jwt.strategy'
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User]),
-    PassportModule.register({defaultStrategy: 'jwt'}),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
-        signOptions: {expiresIn: '3600s'},
+        signOptions: { expiresIn: '3600s' },
       }),
       inject: [ConfigService],
     }),
