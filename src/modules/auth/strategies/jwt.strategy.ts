8cf49aa768800f4a0c5f01008f6ab7232d555796
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-02 18:08:32 +0330

    use fromAuthHeaderAsBearerToken method instead of fromAuthAsBearerToken

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,25 +1,23 @@
-import { Injectable, UnauthorizedException } from "@nestjs/common";
-import { PassportStrategy } from "@nestjs/passport";
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from "../auth.service";
-import { ConfigService } from "@nestjs/config";
-import { JwtPayload } from "../jwt-payload.interface";
+import { AuthService } from '../auth.service';
+import { JwtPayload } from '../jwt-payload.interface';
 
- 
- @Injectable()
- export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor( private readonly authService: AuthService ) {
-        super({
-            jwtFromRequest: ExtractJwt.fromAuthAsBearerToken(),
-            secretOrKey: process.env.JWT_SECRET,
-        });
-    }
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
 
-    async validate(payload: JwtPayload) {
-        const user = await this.authService.validateUserById(payload.sub);
-        if (!user) {
-            throw new UnauthorizedException();
-        }
-        return user;
+  async validate(payload: JwtPayload) {
+    const user = await this.authService.validateUserById(payload.sub);
+    if (!user) {
+      throw new UnauthorizedException();
     }
- }
+    return user;
+  }
+}
