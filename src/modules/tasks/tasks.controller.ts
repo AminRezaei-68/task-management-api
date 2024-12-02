9db7c00dd465070de7c267cc6989f965caed2dc1
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-02 17:25:44 +0330

    add ParseIntPipe to get, patch, delete methods

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,35 +1,48 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
-import { CreateTaskDto } from "./dto/create-task.dto";
-import { UpdateTaskDto } from "./dto/update-task.dto";
-import { TasksService } from "./tasks.service";
-import { PaginationQueryDto } from "../../common/dto/pagination-query.dto";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { TasksService } from './tasks.service';
+import { PaginationQueryDto } from '../../common/dto/pagination-query.dto';
 
 @Controller('tasks')
 export class TasksController {
-    constructor(private readonly tasksService: TasksService){}
+  constructor(private readonly tasksService: TasksService) {}
 
-    @Get()
-    findAll(@Query() paginationQury: PaginationQueryDto){
-        return this.tasksService.findAll(paginationQury);
-    }
+  @Get()
+  findAll(@Query() paginationQury: PaginationQueryDto) {
+    return this.tasksService.findAll(paginationQury);
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: number){
-        return this.tasksService.findOne(id);
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.findOne(id);
+  }
 
-    @Post()
-    create(@Body() createTaskDto: CreateTaskDto){
-        return this.tasksService.create(createTaskDto);
-    }
+  @Post()
+  create(@Body() createTaskDto: CreateTaskDto) {
+    return this.tasksService.create(createTaskDto);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: number, @Body() updateTaskDto: UpdateTaskDto){
-        return this.tasksService.update(id, updateTaskDto);
-    }
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateTaskDto: UpdateTaskDto,
+  ) {
+    return this.tasksService.update(id, updateTaskDto);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: number){
-        return this.tasksService.remove(id);
-    }
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.tasksService.remove(id);
+  }
 }
