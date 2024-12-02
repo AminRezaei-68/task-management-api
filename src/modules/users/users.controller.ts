d10a08efb4315967830bca1364365b7d711521f4
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-02 17:38:46 +0330

    Code tidying

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,34 +1,42 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './entities/dto/create-user.dto';
-import { UpdateUserDto } from './entities/dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly userService: UsersService) {}
+  constructor(private readonly userService: UsersService) {}
 
-    @Post()
-    create(@Body() createUserDto: CreateUserDto) {
-        return this.userService.createUser(createUserDto);
-    }
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
 
-    @Get()
-    async findAll() {
-        return this.userService.findAll();
-    }
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
 
-    @Get(':id')
-    async findOne(@Param('id') id: string) {
-        return this.userService.findOne(id);
-    }
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
 
-    @Patch(':id')
-    async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-        return this.userService.updateUser(id, updateUserDto)
-    }
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
 
-    @Delete(':id')
-    async remove(@Param('id') id: string) {
-        return this.userService.deleteUser(id);
-    }
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return this.userService.deleteUser(id);
+  }
 }
