4ea80c0aca9609a3267a8d9e360d898a96dbc89c
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-02 17:30:05 +0330

    Code tidying

--- src/main.ts
+++ src/main.ts
@@ -10,36 +10,34 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true, 
-      transform: true, 
-      forbidNonWhitelisted: true, 
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
       transformOptions: {
-        enableImplicitConversion: true, 
+        enableImplicitConversion: true,
       },
     }),
   );
 
-    app.useGlobalInterceptors(
-    new WrapResponseInterceptor(), 
-    new TimeoutInterceptor(), 
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
   );
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
   const options = new DocumentBuilder()
-    .setTitle('Task Management API') 
-    .setDescription('API documentation for managing tasks') 
-    .setVersion('1.0') 
-    .addBearerAuth() 
+    .setTitle('Task Management API')
+    .setDescription('API documentation for managing tasks')
+    .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
-  const document = SwaggerModule.createDocument(app, options); 
-  SwaggerModule.setup('api', app, document); 
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}/api`);
-
 }
 bootstrap();
-//ok
