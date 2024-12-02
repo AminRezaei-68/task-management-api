de607d30673d1a575172ab76b31b76cc19a0018e
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-02 18:10:41 +0330

    Code tidying

--- src/modules/auth/dto/auth-credential.dto.ts
+++ src/modules/auth/dto/auth-credential.dto.ts
@@ -1,15 +1,13 @@
-import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
-
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly email: string;
 
-    @IsString()
-    @IsNotEmpty()
-    readonly email: string;
-
-    @IsString()
-    @IsNotEmpty()
-    @MinLength(6)
-    @MaxLength(20)
-    readonly password: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(20)
+  readonly password: string;
 }
