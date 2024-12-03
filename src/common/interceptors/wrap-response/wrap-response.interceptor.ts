67414725779ca7aa22ec2890f5b9e954e032712c
AminRezaei <rezaei.amin1268@gmail.com>
2024-12-03 09:33:30 +0330

    remove description

--- src/common/interceptors/wrap-response/wrap-response.interceptor.ts
+++ src/common/interceptors/wrap-response/wrap-response.interceptor.ts
@@ -10,8 +10,7 @@ import { Observable, map } from 'rxjs';
 export class WrapResponseInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     console.log('before...');
-    
-    return next.handle().pipe(map((data) => ({ data })));
 
+    return next.handle().pipe(map((data) => ({ data })));
   }
 }
