546255f090525db38a41c671ef0ffd39448edb2d
AminRezaei-68 <rezaei.amin1268@gmail.com>
2025-01-11 17:39:34 +0330

    modified jwt strategy

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,23 +1,32 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { AuthService } from '../auth.service';
 import { JwtPayload } from '../jwt-payload.interface';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/modules/users/users.service';
+
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly authService: AuthService) {
+  constructor(private readonly usersService: UsersService,
+    private readonly configService: ConfigService
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_SECRET,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
   async validate(payload: JwtPayload) {
-    const user = await this.authService.validateUserById(payload.sub);
+    const user = await this.usersService.findOne(payload.sub);
+
     if (!user) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('User not found or deactivated');
     }
-    return user;
+    return {
+      userId: payload.sub,
+      username: payload.username,
+    };
   }
 }
